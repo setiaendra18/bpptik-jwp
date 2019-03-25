f2cc33d5b18cf4531ebc298d32890614d1cca367
Endra Setiawan <setiaendra18@gmail.com>
2019-03-25 21:07:34 +0700

    Update for.php

--- 02-PemrogramanTersetruktur/for.php
+++ 02-PemrogramanTersetruktur/for.php
@@ -1,1 +1,38 @@
 
+<?php
+
+function perulanganFor($nilai)
+{
+    for($i=0;$i<=$nilai;$i++)
+    {
+        echo $i;
+    }
+
+    return $nilai;
+}
+function perulangWhile($nilai)
+{
+    $i=0;
+    while($i<=$nilai)
+    {
+        echo $i;
+        $i++;
+    }
+}
+function perulanganDowhile($nilai)
+{
+    $i=0;
+    do
+    {
+        echo $i;
+        $i++;
+    }
+    while($i<=$nilai);
+}
+
+perulanganFor(10);
+echo "</br>";
+perulangWhile(10);
+echo "</br>";
+perulanganDowhile(10);
+?>
