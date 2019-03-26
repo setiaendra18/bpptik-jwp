0aff12f634ea91b0f5ccedbe43f6160c5a5925cc
Endra Setiawan <setiaendra18@gmail.com>
2019-03-26 09:08:24 +0700

    Add files via upload

---
+++ 02-PemrogramanTersetruktur/form_input.php
@@ -0,0 +1,45 @@
+<htmL>
+<head>
+	<title>FORM MAHASISWA</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container margin">
+	<br>
+<div class="row justify-content-md-center">
+	<div class="col-md-5 border border-success shadow p-3 mb-5 bg-white rounded">
+
+		<center><h5 class="text-success">FORM INPUT NILAI MAHASISWA</h5></center>
+		<hr>
+<form action="#">
+  <div class="form-group">
+    <label for="nama"><strong>NAMA :</strong></label>
+
+    <input type="text" class="form-control" id="nama" name="nama">
+  </div>
+
+  <div class="form-group">
+    <label for="tugas"><strong>NILAI TUGAS :</strong></label>
+    <input type="text" class="form-control" id="nama" name="nltugas">
+  </div>
+
+
+  <div class="form-group">
+    <label for="uts"><strong>NILAI UTS</strong></label>
+    <input type="text" class="form-control" id="nama" name="nluts">
+  </div>
+
+
+  <div class="form-group">
+    <label for="uas"><strong>NILAI UAS</strong></label>
+    <input type="text" class="form-control" id="nama" name="nluas">
+  </div>
+  <button type="submit" class="btn btn-success">KIRIM</button>
+  <button type="reset" class="btn btn-danger">RESET</button>
+</div>
+</div>
+</div>
+
+</body>
+</htmL>
