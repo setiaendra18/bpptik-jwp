3fa707e4daec3c838b14ce787a817fdcf151a91b
Endra Setiawan <setiaendra18@gmail.com>
2019-03-27 15:42:21 +0700

    Add files via upload

--- 03-HariTerkahir/date.php
+++ 03-HariTerkahir/date.php
@@ -3,24 +3,41 @@ require_once "vendor/autoload.php";
 
 use Carbon\Carbon;
 
+/*
+*@var $today mencetak waktu hari ini
+*/
 $today=Carbon::now()->toDateTimeString();
 echo "Waktu hari ini : ".$today."</br>";
 
+/*
+*@var $jakarta mencetak tanggal dan waktu dengan zona Asia/Jakarta
+*/
 $jakarta=Carbon::now('Asia/Jakarta');
 echo "Waktu Asia/Jakarta : ".$jakarta."</br>";
-
+/*
+*@var $besok mencetak tanggal dan waktu untuk besok dengan zona Asia/Jakarta
+*/
 $besok = Carbon::now('Asia/Jakarta')->addDay();
 echo "Waktu Asia/Jakarta Besok : ".$besok."</br>";
-
+/*
+*@var $parsing mencetak tanggal dan waktu dengan zona Asia/Jakarta dan di konversi 
+*pada format standar di indonesia
+*/
 $parsing = Carbon::parse($today)->locale('id_ID')->isoFormat('LLLL');
 echo "Rubah Format : ".$parsing."</br>";
-
+/*
+*@var $umur mencetak berapa tahu Indonesai berdisi
+*/
 $umur = Carbon::createFromDate(1945, 8, 17)->age;
 echo "Umur Indonesia : ".$umur." Tahun </br>";
-
+/*
+*@var $namazona mencetak hari dengan zona waktu Asia/Jakarta
+*/
 $namazona=Carbon::parse($today, 'Asia/Jakarta')->timezone->getName();
 echo "Nama Zona Waktu : ".$namazona."</br>";
-
+/*
+*@var $tahun mencetak selisih tahun
+*/
 $tahun=Carbon::create($umur)->diffForHumans();
 echo "Tahun : ".$tahun."</br>";
 
--- 03-HariTerkahir/random.php
+++ 03-HariTerkahir/random.php
@@ -1,14 +1,26 @@
 <?php
 require_once "vendor/autoload.php";
-
 $factory = new RandomLib\Factory;
+/*
+*@var $generator inialisasi objek dengan menggunakan library pada variabel dan mencetak random
+string menggunakan kata BPPTIK sebanyak 32 karakte 
+*/
 $generator = $factory->getGenerator(new SecurityLib\Strength(SecurityLib\Strength::MEDIUM));
 print $generator->generateString(32, 'BPPTIK')."\n";
 echo "</br>";
+/*
+*@var $generator inialisasi objek dengan menggunakan library pada variabel dan mencetak random
+string menggunakan kata MAJU sebanyak 16 karakter
+*/
 $generator = $factory->getLowStrengthGenerator();
-print $generator->generateString(32, 'BPPTIK')."\n";
+print $generator->generateString(16, 'MAJU')."\n";
 echo "</br>";
+/*
+*@var $generator inialisasi objek dengan menggunakan library pada variabel dan mencetak random
+integer dalam range angka 1 sampai dengan 100;
+*/
 $generator = $factory->getLowStrengthGenerator();
 print $randomInt = $generator->generateInt(1, 100);
 echo "</br>";
+
 ?>
